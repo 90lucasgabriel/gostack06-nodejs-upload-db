2c9a05e07f29ebb48dd304754ded8e0d3fa495a3
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-04-25 13:28:40 -0300

    - New: get transactions;

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import { getCustomRepository } from 'typeorm';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const repository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await repository.find();
+  const balance = await repository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
